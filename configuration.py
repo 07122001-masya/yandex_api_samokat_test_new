3d53fe17f817658f3c10dc543a6d7abe14820639
07122001-masya <>
2025-12-03 18:12:13 +0300

    Changes have been made based on the reviewer's comments

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE  = "https://0f03e847-5ebe-49f4-9804-34bac1f37c82.serverhub.praktikum-services.ru/"
+URL_SERVICE  = "https://bf9b6f8f-fba6-4f38-8c05-0ab6092236b8.serverhub.praktikum-services.ru"
 CREATE_ORDER_PATH = "/api/v1/orders/" 
 TRACK_ORDER_PATH = "/api/v1/orders/track"
